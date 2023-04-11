c4b9606af7ca55319d3a9a73d7bc1ce1244ec2d0
Camarederic <camarederic@mail.ru>
2023-04-11 17:42:51 +0300

    Create Fragments, Requirements and fragment_introduction.xml

---
+++ app/src/main/java/com/example/ecommerceappexample/fragments/loginregister/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerceappexample.fragments.loginregister
+
+import androidx.fragment.app.Fragment
+import com.example.ecommerceappexample.R
+
+class AccountOptionsFragment:Fragment(R.layout.fragment_account_options) {
+}
---
+++ app/src/main/java/com/example/ecommerceappexample/fragments/loginregister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerceappexample.fragments.loginregister
+
+import androidx.fragment.app.Fragment
+import com.example.ecommerceappexample.R
+
+class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
+}
