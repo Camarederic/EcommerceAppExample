c041c853721c70f45bc3e01214d68e6c83cefcd6
Camarederic <camarederic@mail.ru>
2023-04-13 18:13:02 +0300

    Create the BottomNavigation, four fragments: HomeFragment, SearchFragment, CartFragment and ProfileFragment

--- app/src/main/java/com/example/ecommerceappexample/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/ecommerceappexample/activities/ShoppingActivity.kt
@@ -2,12 +2,24 @@ package com.example.ecommerceappexample.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.ecommerceappexample.R
+import com.example.ecommerceappexample.databinding.ActivityShoppingBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ShoppingActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityShoppingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        binding = ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
